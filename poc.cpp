5d6d3db0d164346d7f92772274188fc677bdc948
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-11 20:15:43 +0100

    one more face

--- poc.cpp
+++ poc.cpp
@@ -32,7 +32,7 @@ struct vtx {
   dotz::vec3 tgt;
 };
 
-static constexpr const auto vtx_count = 12;
+static constexpr const auto vtx_count = 18;
 static auto load_cube(vee::physical_device pd) {
   voo::h2l_buffer buf { pd, sizeof(vtx) * vtx_count };
   voo::memiter<vtx> m { buf.host_memory() };
@@ -52,6 +52,14 @@ static auto load_cube(vee::physical_device pd) {
   m += { .pos {  1, 1,  1 }, .uv { 1, 1 }, .nrm { 0, 1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
   m += { .pos { -1, 1, -1 }, .uv { 0, 0 }, .nrm { 0, 1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
   m += { .pos { -1, 1,  1 }, .uv { 0, 1 }, .nrm { 0, 1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
+
+  m += { .pos {  1, -1,  1 }, .uv { 1, 1 }, .nrm { 0, -1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
+  m += { .pos { -1, -1, -1 }, .uv { 0, 0 }, .nrm { 0, -1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
+  m += { .pos {  1, -1, -1 }, .uv { 1, 0 }, .nrm { 0, -1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
+
+  m += { .pos {  1, -1,  1 }, .uv { 1, 1 }, .nrm { 0, -1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
+  m += { .pos { -1, -1,  1 }, .uv { 0, 1 }, .nrm { 0, -1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
+  m += { .pos { -1, -1, -1 }, .uv { 0, 0 }, .nrm { 0, -1, 0 }, .btgt { 0, 0, 1 }, .tgt { 1, 0, 0 } };
 
   return buf;
 }
